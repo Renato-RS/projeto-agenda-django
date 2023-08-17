1c335186f6d66c523136e04f319cf2865af488fc
renato <renato.rodrigues.si@hotmail.com>
2023-08-17 14:19:11 -0300

    Criando header e partial files

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -7,7 +7,8 @@ def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
 
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos - ',
     }
 
     return render(
@@ -22,8 +23,10 @@ def contact(request, contact_id):
         Contact, pk=contact_id, show=True
     )
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
     context = {
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title': site_title,
     }
 
     return render(
