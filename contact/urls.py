4eb5547ea40922d8cc3417dfd472169e848caa15
renato <renato.rodrigues.si@hotmail.com>
2023-08-18 20:43:59 -0300

    url, view, template do formulario create

--- contact/urls.py
+++ contact/urls.py
@@ -3,7 +3,8 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('create/', views.create, name='create'),
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),
 ]
