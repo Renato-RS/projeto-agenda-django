c08d64170e94ef25ea2d6394eb09f18b9c6af3dd
renato <renato.rodrigues.si@hotmail.com>
2023-08-28 10:48:06 -0300

    sistema de login e logout de usuarios

--- contact/urls.py
+++ contact/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
     
-    #Register
+    #User
     path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+
 ]
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from contact.forms import RegisterForm
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.forms import AuthenticationForm
+
 
 def register(request):
     form = RegisterForm()
@@ -14,10 +16,36 @@ def register(request):
             return redirect('contact:index')
 
     return render(
-            request,
-            'contact/register.html',
-            {
-                'form': form
-            }
-        )
-        
+        request,
+        'contact/register.html',
+        {
+            'form': form
+        }
+    )
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Logged in succesfully')
+            return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+    messages.success(request, 'Logout in succesfully')
+    return redirect('contact:login')
