c983efd963601a14f73629c0512596e5c75288ec
renato <renato.rodrigues.si@hotmail.com>
2023-08-29 14:46:33 -0300

    diferenciando interface e permissões de usuários logados e não logados

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -3,16 +3,18 @@ from contact.forms import ContactForm
 from django.urls import reverse
 from contact.models import Contact
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
     if request.method == 'POST':
-        
+
         form = ContactForm(request.POST, request.FILES)
         context = {
             'form': form,
-            'form_action' : form_action,
+            'form_action': form_action,
         }
 
         if form.is_valid():
@@ -38,18 +40,19 @@ def create(request):
     )
 
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True,
-        )
+    )
     form_action = reverse('contact:update', args=(contact_id,))
-    
+
     if request.method == 'POST':
-        
+
         form = ContactForm(request.POST, request.FILES, instance=contact)
         context = {
             'form': form,
-            'form_action' : form_action,
+            'form_action': form_action,
         }
 
         if form.is_valid():
@@ -74,6 +77,8 @@ def update(request, contact_id):
         context
     )
 
+
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
